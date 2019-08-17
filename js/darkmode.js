f30dfb74588f551ff1716a99bd4e5a61f1275ee4
Евгений <quemsaurose@gmail.com>
2019-08-17 23:03:36 +0300

    всего по чуть-чуть

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,6 +1,5 @@
 $(function () {
   $('#dark-mode').on('click', function (e) {
-
     e.preventDefault();
     $('h4, button').not('.check').toggleClass('text-white');
     $('.list-panel a').toggleClass('dark-grey-text');
@@ -13,9 +12,12 @@ $(function () {
     $('.btn-dash').toggleClass('grey blue').toggleClass('lighten-3 darken-3');
     $('.gradient-card-header').toggleClass('white black lighten-4');
     $('.list-panel a').toggleClass('navy-blue-bg-a text-white').toggleClass('list-group-border');
-    $('.card, .card-rating').toggleClass('dark-card-admin');
+    $('.card, .card-rating, .card-favourite, .card-watch-later').toggleClass('dark-card-admin');
     $('.card-rating a').toggleClass('text-white');
+    $('.card-favourite a').toggleClass('text-white');
+    $('.card-watch-later a').toggleClass('text-white');
+    $('.dropdown-content').toggleClass('dark-card-admin');
+    $('.dropdown-content li > a, .dropdown-content li > span').toggleClass('text-white');
     $('#dark-mode').toggleClass('dark-card-admin').removeClass('text-dark');
-
   });
 });
