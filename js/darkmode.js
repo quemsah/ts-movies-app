a8655d5815c2aefd833094835b741094ff2d7827
Евгений <quemsaurose@gmail.com>
2019-08-26 03:33:03 +0300

    edit btn & mail btn

--- js/darkmode.js
+++ js/darkmode.js
@@ -27,7 +27,7 @@
    $('i.far.fa-thumbs-down').removeClass('text-white');
    $('i.far.fa-trash-alt').removeClass('text-white');
    //$('i.far.fa-edit').removeClass('text-white');
-   $('i.far.fa-edit').toggleClass('blue-text');
+   $('.comment-btns a i.far.fa-edit').toggleClass('blue-text');
    //
    $('.select-rating').toggleClass('text-white');
    $('.select-option').toggleClass('dark-card-admin');
